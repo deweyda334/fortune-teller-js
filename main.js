fcbccbe994719c78f5c5e339abd978ecf0215fa4
deweyda334 <153093259+deweyda334@users.noreply.github.com>
2024-04-30 14:01:50 -0400

    updated script

--- main.js
+++ main.js
@@ -2,6 +2,34 @@
 // 4/30/2024
 // JS Fortune Teller Project
 
+const countries = [
+    "United States",
+    "Canada",
+    "Zimbabwe",
+    "Yemen",
+    "Denmark",
+    "Qatar",
+];
+
+const jobs = [
+    "Walmart greeter",
+    "mechanic",
+    "chef",
+    "pilot",
+    "doctor",
+    "teacher",
+    "lumberjack",
+    "librarian",
+];
+
+const randCountry = countries[Math.floor(Math.random( ) * countries.length)];
+
+const randJob = jobs[Math.floor(Math.random( ) * jobs.length)];
+
+
+
+
+
 
 
 
